649150cda875a218e8acc99192c46a5fb3ab081e
thomas bartier <thomas.bartier@gmail.com>
2026-05-08 22:02:48 +0200

    add auth page ui + update index css + add router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
