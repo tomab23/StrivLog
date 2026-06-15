c6bf3047cbe581275e185fee7581ec6c10252e5f
thomas bartier <thomas.bartier@gmail.com>
2026-06-15 14:24:14 +0200

    add activity model + tsconfig update

---
+++ src/models/Activity.ts
@@ -0,0 +1,35 @@
+// date - hour - duration - distance => km - calories => kcal - note
+
+export default class Activity {
+  id: number
+  created_at: string
+  date: string
+  hour: string
+  duration: number
+  distance: number
+  calories: number
+  note: string | null
+  type: string
+
+  constructor(
+    id: number,
+    created_at: string,
+    date: string,
+    hour: string,
+    duration: number,
+    distance: number,
+    calories: number,
+    note: string | null,
+    type: string
+  ) {
+    this.id = id
+    this.created_at = created_at
+    this.date = date
+    this.hour = hour
+    this.duration = duration
+    this.distance = distance
+    this.calories = calories
+    this.note = note
+    this.type = type
+  }
+}
